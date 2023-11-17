1db1cc65f31dc967650e5590df717df8190f88f5
j.guidoux <j.guidoux@groupe-creative.fr>
2023-11-18 00:10:24 +0100

    Connection between Publication and reservation context (asynchronous version)

--- src/main/java/com/zenika/training/freenb/publishing/infra/OfferPublishedJava.java
+++ src/main/java/com/zenika/training/freenb/publishing/infra/OfferPublishedJava.java
@@ -3,20 +3,21 @@ package com.zenika.training.freenb.publishing.infra;
 import com.zenika.training.freenb.publishing.domain.OfferPublisher;
 import com.zenika.training.freenb.reservation.api.OfferPublished;
 import com.zenika.training.freenb.reservation.api.OfferPublishedConsumer;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfferPublishedJava implements OfferPublisher {
 
-    private final OfferPublishedConsumer consumer;
+    private final ApplicationEventPublisher publisher;
 
-    public OfferPublishedJava(OfferPublishedConsumer consumer) {
-        this.consumer = consumer;
+    public OfferPublishedJava(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
     public void publish(OfferPublished offerPublished) {
-        consumer.receive(offerPublished);
+        publisher.publishEvent(offerPublished);
 
     }
 }
--- src/main/java/com/zenika/training/freenb/reservation/api/OfferPublishedConsumer.java
+++ src/main/java/com/zenika/training/freenb/reservation/api/OfferPublishedConsumer.java
@@ -3,16 +3,19 @@ package com.zenika.training.freenb.reservation.api;
 import com.zenika.training.freenb.reservation.application.AddNewAvailableOffer;
 import com.zenika.training.freenb.reservation.domain.AvailableOffer;
 import com.zenika.training.freenb.reservation.domain.OfferId;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfferPublishedConsumer {
+
     private final AddNewAvailableOffer service;
 
     public OfferPublishedConsumer(AddNewAvailableOffer service) {
         this.service = service;
     }
 
+    @EventListener
     public void receive(OfferPublished offerPublished) {
         AvailableOffer availableOffers = new AvailableOffer(new OfferId(offerPublished.value()));
         service.execute(availableOffers);
